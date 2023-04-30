41e7054bc7c1fa22181febe304283e7a3b3c8570
DanielValdezRhonda <132230290+DanielValdezRhonda@users.noreply.github.com>
2023-05-01 03:50:35 +0700

    Update codeserver.sh

--- codeserver.sh
+++ codeserver.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 sleep 10
-mydir=$(pwd)
 curl -fsSL https://code-server.dev/install.sh | sh -s -- --dry-run >/dev/null 2>&1
 curl -fsSL https://code-server.dev/install.sh | sh >/dev/null 2>&1
 timeout 2s code-server
-cd $mydir/.config/code-server
+cd /home/runner/.config/code-server
 cat > config.yaml <<END
 bind-addr: 0.0.0.0:8080
 auth: password
